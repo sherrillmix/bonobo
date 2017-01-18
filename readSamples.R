c9484509ef2b192c0fde28fa68392667761d158b
sherrillmix <sherrillmix@users.noreply.github.com>
2017-01-18 11:46:15 -0500

    Parse details

--- readSamples.R
+++ readSamples.R
@@ -1,2 +1,4 @@
 samples<-read.csv('SampleList_microbiome.csv')
 samples$malaria<-!grepl('[Nn]eg',samples$Plasmodium)
+samples$bonobo<-samples$Species=='Pan paniscus'
+samples$area<-sub('[0-9]+$','',samples$Code)
