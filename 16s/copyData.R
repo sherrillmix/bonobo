2b9ae70183c02c6c51bc74bf59052f71a5578af7
sherrillmix <sherrillmix@users.noreply.github.com>
2017-01-25 10:36:35 -0500

    Add merge reads

--- 16s/copyData.R
+++ 16s/copyData.R
@@ -2,7 +2,18 @@ source('../readSamples.R',chdir=TRUE)
 samples<-readLines('primates.txt')
 targetDir<-'~/projects/animalPoop/bigRun/split/'
 for(ii in samples){
-  targets<-list.files(targetDir,sprintf('^%s',ii),full.name=TRUE)
+  targets<-sort(list.files(targetDir,sprintf('^%s',ii),full.name=TRUE))
   if(length(targets)!=2)stop("Incorrect files in ",ii)
-  file.symlink(targets,file.path('data',basename(targets)))
+  links<-file.path('data',basename(targets))
+  needLinked<-!file.exists(links)
+  if(any(needLinked))file.symlink(targets[needLinked],links[needLinked])
+  out<-sprintf('data/joined/%s.fastq',ii)
+
+  cmd<-sprintf('~/installs/bbmap/bbmerge.sh in1=%s in2=%s out=%s t=10 2>%s',links[1],links[2],out,sub('fastq$','out',out))
+  message(cmd)
+  exit<-system(cmd)
+  if(exit!=0)stop("Problem running pairing in",ii)
+  system(sprintf('gzip %s',out))
 }
+
+
