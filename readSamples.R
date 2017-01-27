bd4cfc753f4f530c79218ae70b258caf78576610
sherrillmix <sherrillmix@users.noreply.github.com>
2017-01-27 15:36:29 -0500

    No factors. Split west and east TL

--- readSamples.R
+++ readSamples.R
@@ -1,4 +1,6 @@
-samples<-read.csv('SampleList_microbiome.csv')
+samples<-read.csv('SampleList_microbiome.csv',stringsAsFactors=FALSE)
 samples$malaria<-!grepl('[Nn]eg',samples$Plasmodium)
 samples$bonobo<-samples$Species=='Pan paniscus'
 samples$area<-sub('[0-9]+$','',samples$Code)
+samples$area[samples$Site=='TL']<-'TL-E'
+samples$area[samples$Site=='TL (West)']<-'TL-W'
