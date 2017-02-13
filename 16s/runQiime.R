ef4e0809152199e1870a4779500b63369ab855d0
sherrillmix <sherrillmix@users.noreply.github.com>
2017-02-13 10:36:25 -0500

    Fix bifurcating tree. Add colors for TL and KR

--- 16s/runQiime.R
+++ 16s/runQiime.R
@@ -55,12 +55,16 @@ otuTab<-as.data.frame.matrix(table(otus$otu,otus$file))
 otuTab<-otuTab[apply(otuTab,1,sum)>3,]
 
 samples$name<-sapply(samples$Code,function(x)colnames(otuTab)[grep(sprintf('_%s$',x),colnames(otuTab))])
+rownames(samples)<-samples$name
+#make sure same order as otu table
+if(any(!samples$name %in% colnames(otuTab)))stop('Sample missing from OTUs')
+samples<-samples[colnames(otuTab)[colnames(otuTab) %in% samples$name],]
 
 library(phyloseq)
-tree<-read_tree('work/qiime/rep_set.tre')
+tree<-multi2di(read_tree('work/qiime/rep_set.tre'))
 phyOtu<-otu_table(otuTab[rownames(otuTab) %in% tree$tip.label,samples$name],taxa_are_rows=TRUE)
 qiimeData<-phyloseq(otu_table=phyOtu,phy_tree=tree)
-#check warning here
+#make sure tree is bifurcating or breaks UniFrac without error
 uniDist<-UniFrac(qiimeData,weighted=TRUE)
 library(ape)
 uniPca<-pcoa(uniDist)
@@ -70,6 +74,7 @@ colnames(predictors)[colnames(predictors)=='malariaTRUE']<-'malariaPos'
 
 
 #areaCols<-rainbow.lab(length(unique(samples$area)),alpha=.8)
+source('myBiplot.R')
 colorBrew<-c('#e41a1cBB','#377eb8BB','#4daf4aBB','#984ea3BB','#ff7f00BB','#ffff33BB','#a65628BB','#f781bfBB','#999999BB')
 nArea<-length(unique(samples$area))
 if(nArea>length(colorBrew))stop('Need to adjust colors for more areas')
@@ -82,6 +87,10 @@ names(malariaCols2)<-names(malariaCols)<-c('PlasmoNeg','PlasmoPos')
 speciesPch<-20+1:length(unique(samples$Species))
 speciesCols<-rainbow.lab(length(unique(samples$Species)),start=-2,end=2,alpha=.9,lightMultiple=.8)
 names(speciesCols)<-names(speciesPch)<-unique(samples$Species)
+#split out TL2-E and -W, bonobos and KR and chimps
+splitAreas<-ifelse(samples$area %in% c('TL-E','TL-W','KR'),samples$area,samples$Species)
+splitAreaCols<-rainbow.lab(length(unique(splitAreas)))
+names(splitAreaCols)<-unique(splitAreas)[order(unique(splitAreas) %in% c('P.t. schweinfurthii'))]
 pdf('out/pcoa.pdf',height=9,width=9)
   sapply(list(1:2,3:4,5:6),function(axes){
     pos<-my.biplot.pcoa(uniPca,predictors,plot.axes=axes,pch=speciesPch[samples$Species],bg=areaCols[samples$area],col=malariaCols[samples$malaria+1],cex=2.2,lwd=2.5)
@@ -105,6 +114,10 @@ pdf('out/pcoa.pdf',height=9,width=9)
     pos<-my.biplot.pcoa(uniPca,predictors,plot.axes=axes,pch=21,bg=speciesCols[samples$Species],col=malariaCols[samples$malaria+1],cex=2.25,lwd=4)
     legend('bottomright',as.vector(outer(names(speciesCols),names(malariaCols),paste,sep=' ')),col=as.vector(malariaCols[outer(names(speciesCols),names(malariaCols),function(x,y)y)]),pch=21,pt.bg=as.vector(speciesCols[outer(names(speciesCols),names(malariaCols),function(x,y)x)]),inset=.01,pt.lwd=4,pt.cex=2.5)
     title(main=sprintf('Species/malaria PC %d and %d',axes[1],axes[2]))
+    pos<-my.biplot.pcoa(uniPca,predictors,plot.axes=axes,pch=21,bg=splitAreaCols[splitAreas],col=malariaCols[1],cex=2.25,lwd=4)
+    legend('bottomright',names(splitAreaCols),col=malariaCols[1],pch=21,pt.bg=splitAreaCols,inset=.01,pt.lwd=4,pt.cex=2.5)
+    title(main=sprintf('TL and KR PC %d and %d',axes[1],axes[2]))
+    #show sample name text
     #show sample name text
     #bak<-par('cex')
     #par('cex'=.65)
