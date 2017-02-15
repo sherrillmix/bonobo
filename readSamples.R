d8ac7b27f619106f603ef5b85e1d6fd89bf7a24a
sherrillmix <sherrillmix@users.noreply.github.com>
2017-02-15 16:25:28 -0500

    Get months

--- readSamples.R
+++ readSamples.R
@@ -1,6 +1,12 @@
+library(lubridate)
 samples<-read.csv('SampleList_microbiome.csv',stringsAsFactors=FALSE)
 samples$malaria<-!grepl('[Nn]eg',samples$Plasmodium)
 samples$bonobo<-samples$Species=='Pan paniscus'
 samples$area<-sub('[0-9]+$','',samples$Code)
 samples$area[samples$Site=='TL']<-'TL-E'
 samples$area[samples$Site=='TL (West)']<-'TL-W'
+samples$rDate<-mdy(samples$Date)
+samples$month<-month(samples$rDate)
+#arbitrary seasons
+samples$season<-as.character(ceiling(samples$month/3))
+
