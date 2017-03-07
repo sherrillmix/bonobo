159c4428b4b6bcc48c32e65ca883371c553689a9
sherrillmix <sherrillmix@users.noreply.github.com>
2017-03-07 12:49:05 -0500

    Move runSwarm to functions

--- functions.R
+++ functions.R
@@ -51,3 +51,23 @@ parseQiimeTaxa<-function(taxas,desiredTaxa=c('k','p','c','o','f','g','s'),concat
 }
 
 
+runSwarm<-function(seqs,swarmBin='swarm',swarmArgs='-f'){
+  if(any(is.na(seqs)))stop(simpleError('NAs in seqs'))
+  seqIds<-as.numeric(as.factor(seqs))
+  seqCounts<-ave(seqIds,seqIds,FUN=length)
+  seqNames<-sprintf('%08d_%d',seqIds,seqCounts)
+  readFile<-tempfile()
+  outFile<-tempfile()
+  seqFile<-tempfile()
+  uniqSelector<-!duplicated(seqs)
+  write.fa(seqNames[uniqSelector],seqs[uniqSelector],readFile)
+  cmd<-sprintf('%s %s %s -o %s -w %s',swarmBin,swarmArgs,readFile,outFile,seqFile)
+  system(cmd)
+  swarm<-readLines(outFile)
+  otuSplit<-strsplit(swarm,' ')
+  otus<-rep(1:length(otuSplit),sapply(otuSplit,length))
+  names(otus)<-unlist(otuSplit)  
+  out<-otus[seqNames]
+  seedSeqs<-read.fa(seqFile)
+  return(list('otus'=out,'seqs'=seedSeqs))
+}
--- makeOtus.R
+++ makeOtus.R
@@ -1,39 +1,25 @@
 library(dnar)
 library(parallel)
+source('functions.R')
 
 fastqs<-list.files('data','_R[12]_.*\\.fastq\\.gz$',recursive=TRUE,full.names=TRUE)
 fastqs<-fastqs[!grepl('Undetermined',fastqs)]
 primers<-sub('.*(matK|rbcL).*_R([0-9]+)_.*','\\1\\2',basename(fastqs))
 
-runSwarm<-function(seqs,swarmBin='swarm',swarmArgs='-f'){
-  if(any(is.na(seqs)))stop(simpleError('NAs in seqs'))
-  seqIds<-as.numeric(as.factor(seqs))
-  seqCounts<-ave(seqIds,seqIds,FUN=length)
-  seqNames<-sprintf('%08d_%d',seqIds,seqCounts)
-  readFile<-tempfile()
-  outFile<-tempfile()
-  seqFile<-tempfile()
-  uniqSelector<-!duplicated(seqs)
-  write.fa(seqNames[uniqSelector],seqs[uniqSelector],readFile)
-  cmd<-sprintf('%s %s %s -o %s -w %s',swarmBin,swarmArgs,readFile,outFile,seqFile)
-  system(cmd)
-  swarm<-readLines(outFile)
-  otuSplit<-strsplit(swarm,' ')
-  otus<-rep(1:length(otuSplit),sapply(otuSplit,length))
-  names(otus)<-unlist(otuSplit)  
-  out<-otus[seqNames]
-  seedSeqs<-read.fa(seqFile)
-  return(list('otus'=out,'seqs'=seedSeqs))
-}
 
 for(ii in unique(primers)){
   message(ii)
   thisFiles<-fastqs[primers==ii]
-  reads<-cleanMclapply(thisFiles,function(xx){library(dnar);cat('.');read.fastq(xx)$seq},mc.cores=5)
-  reads<-lapply(reads,function(xx)xx[!grepl('[^ACTG]',xx)])
+  reads<-cleanMclapply(thisFiles,function(xx){library(dnar);cat('.');read.fastq(xx)$seq},mc.cores=7)
+  #WARNING cutting reads 
+  reads<-lapply(reads,function(xx)substring(xx[!grepl('[^ACTG]',xx)],20,200))
   samples<-rep(basename(thisFiles),sapply(reads,length))
   otus<-runSwarm(unlist(reads),'~/installs/swarm/swarm',swarmArgs='-f -t 30')
   browser()
+  outDir<-sprintf('work/qiime_%s',ii)
+  if(!dir.exists(outDir)){
+    out<-runQiime(unlist(reads),storeDir=outDir)
+  }
   #write.csv(cbind(samples,'otu'=otus[['otus']]),sprintf('work/swarm_%s.csv',ii))
   #write.csv(cbind(samples,otus[['otus']]),sprintf('work/swarm_%s.fa'))
 }
