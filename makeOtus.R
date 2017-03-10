37311eb1706a912e7940ec6543af586f4ba8ce75
sherrillmix <sherrillmix@users.noreply.github.com>
2017-03-10 15:01:27 -0500

    Trim reads

--- makeOtus.R
+++ makeOtus.R
@@ -2,7 +2,7 @@ library(dnar)
 library(parallel)
 source('functions.R')
 
-fastqs<-list.files('data','_R[12]_.*\\.fastq\\.gz$',recursive=TRUE,full.names=TRUE)
+fastqs<-list.files('data/Weimin_Plants_3_6_17/','_R[12]_.*\\.fastq\\.gz$',recursive=TRUE,full.names=TRUE)
 fastqs<-fastqs[!grepl('Undetermined',fastqs)]
 primers<-sub('.*(matK|rbcL).*_R([0-9]+)_.*','\\1\\2',basename(fastqs))
 
@@ -11,11 +11,11 @@ for(ii in unique(primers)){
   message(ii)
   thisPrimer<-primerSeqs[[sub('[12]$','',tolower(ii))]][as.numeric(substring(ii,nchar(ii)))]
   thisFiles<-fastqs[primers==ii]
-  reads<-cleanMclapply(thisFiles,function(xx){library(dnar);cat('.');read.fastq(xx)$seq},mc.cores=7)
+  reads<-mclapply(thisFiles,function(xx){library(dnar);cat('.');read.fastq(xx)$seq},mc.cores=6,mc.preschedule=FALSE)
   #WARNING cutting reads 
-  reads<-lapply(reads,function(xx)substring(xx[!grepl('[^ACTG]',xx)],nchar(thisPrimer)+1,200))
-  samples<-rep(basename(thisFiles),sapply(reads,length))
-  otus<-runSwarm(unlist(reads),'~/installs/swarm/swarm',swarmArgs='-f -t 30')
+  trimReads<-lapply(reads,function(xx)substring(xx[!grepl('[^ACTG]',xx)],nchar(thisPrimer)+1,200))
+  samples<-rep(basename(thisFiles),sapply(trimReads,length))
+  otus<-runSwarm(unlist(trimReads),'~/installs/swarm/swarm',swarmArgs='-f -t 30')
   browser()
   outDir<-sprintf('work/qiime_%s',ii)
   if(!dir.exists(outDir)){
