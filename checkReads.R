4b2a6e5978ec41994eaaa6b9eb6c99825cd5382d
sherrillmix <sherrillmix@users.noreply.github.com>
2017-03-13 15:51:46 -0400

    Check new quals

--- checkReads.R
+++ checkReads.R
@@ -7,7 +7,7 @@ library(parallel)
 fastqs<-list.files('data/','_R[12]_.*\\.fastq\\.gz$',recursive=TRUE,full.names=TRUE)
 fastqs<-fastqs[!grepl('Undetermined',fastqs)]
 allQs<-mclapply(fastqs,function(xx){
-  library(dnar)
+  cat('.')
   seqs<-read.fastq(xx)
   if(nrow(seqs)<500)return(NULL)
   quals<-do.call(rbind,qualToInts(seqs$qual))
@@ -16,8 +16,8 @@ allQs<-mclapply(fastqs,function(xx){
 names(allQs)<-fastqs
 
 fastqs16s<-list.files('16s/data/','_[12]\\.fastq\\.gz$',recursive=TRUE,full.names=TRUE)
-allQs16s<-cleanMclapply(fastqs16s,function(xx){
-  library(dnar)
+allQs16s<-mclapply(fastqs16s,function(xx){
+  cat('.')
   seqs<-read.fastq(xx)
   if(nrow(seqs)<500)return(NULL)
   quals<-do.call(rbind,qualToInts(seqs$qual))
@@ -25,23 +25,27 @@ allQs16s<-cleanMclapply(fastqs16s,function(xx){
 },mc.cores=10)
 names(allQs16s)<-fastqs16s
 
-r1s<-allQs[grepl('_R1_',names(allQs))&!grepl('Weimin_Plants_3_6_17',names(allQs))]
-r2s<-allQs[grepl('_R2_',names(allQs))&!grepl('Weimin_Plants_3_6_17',names(allQs))]
+r1s<-allQs[grepl('_R1_',names(allQs))&!grepl('Weimin(_Plants)?_3_[0-9]+_17',names(allQs))]
+r2s<-allQs[grepl('_R2_',names(allQs))&!grepl('Weimin(_Plants)?_3_[0-9]+_17',names(allQs))]
 r1s16s<-allQs16s[!grepl('/joined/',names(allQs16s))&grepl('_1\\.fastq',names(allQs16s))]
 r2s16s<-allQs16s[!grepl('/joined/',names(allQs16s))&grepl('_2\\.fastq',names(allQs16s))]
 newR1s<-allQs[grepl('_R1_',names(allQs))&grepl('Weimin_Plants_3_6_17',names(allQs))]
 newR2s<-allQs[grepl('_R2_',names(allQs))&grepl('Weimin_Plants_3_6_17',names(allQs))]
+newR1s_250<-allQs[grepl('_R1_',names(allQs))&grepl('Weimin_3_13_17',names(allQs))]
+newR2s_250<-allQs[grepl('_R2_',names(allQs))&grepl('Weimin_3_13_17',names(allQs))]
 
 pdf('out/quals.pdf')
   plot(1,1,type='n',ylim=c(0,40),xlim=c(1,length(allQs[[1]])),main='All',xlab='Position',ylab='Average quality')
-  lines(apply(do.call(rbind,r1s),2,mean),col='red',lwd=2)
-  lines(apply(do.call(rbind,r2s),2,mean),col='orange',lwd=2)
-  lines(apply(do.call(rbind,r1s16s),2,mean),col='purple',lwd=2)
-  lines(apply(do.call(rbind,r2s16s),2,mean),col='blue',lwd=2)
+  lines(apply(do.call(rbind,r1s),2,mean),col='blue',lwd=2)
+  lines(apply(do.call(rbind,r2s),2,mean),col='darkblue',lwd=2)
+  lines(apply(do.call(rbind,r1s16s),2,mean),col='grey',lwd=2)
+  lines(apply(do.call(rbind,r2s16s),2,mean),col='darkgrey',lwd=2)
   lines(apply(do.call(rbind,newR1s),2,mean),col='green',lwd=2)
-  lines(apply(do.call(rbind,newR2s),2,mean),col='cyan',lwd=2)
+  lines(apply(do.call(rbind,newR2s),2,mean),col='darkgreen',lwd=2)
+  lines(apply(do.call(rbind,newR1s_250),2,mean),col='red',lwd=2)
+  lines(apply(do.call(rbind,newR2s_250),2,mean),col='darkred',lwd=2)
   abline(h=30,lty=2)
-  legend('bottomleft',c('Chloro 1','Chloro 2','16S 1','16S 2','New chloro 1','New chloro 2'),col=c('red','orange','purple','blue','green','cyan'),lty=1,inset=.01,lwd=2)
+  legend('bottomleft',c('Chloro 1','Chloro 2','16S 1','16S 2','New chloro 1','New chloro 2','250 chloro 1','250 chloro 2'),col=c('blue','darkblue','grey','darkgrey','green','darkgreen','red','darkred'),lty=1,inset=.01,lwd=2)
   plot(1,1,type='n',ylim=c(0,40),xlim=c(1,length(allQs[[1]])),main='Chloro Read 1',xlab='Position',ylab='Average quality per sample')
   lapply(r1s,function(xx)lines(1:length(xx),xx,col='#00000044'))
   plot(1,1,type='n',ylim=c(0,40),xlim=c(1,length(allQs[[1]])),main='Chloro Read 2',xlab='Position',ylab='Average quality per sample')
@@ -54,6 +58,10 @@ pdf('out/quals.pdf')
   lapply(newR1s,function(xx)lines(1:length(xx),xx,col='#00000044'))
   plot(1,1,type='n',ylim=c(0,40),xlim=c(1,length(allQs[[1]])),main='New chloro Read 2',xlab='Position',ylab='Average quality per sample')
   lapply(newR2s,function(xx)lines(1:length(xx),xx,col='#00000044'))
+  plot(1,1,type='n',ylim=c(0,40),xlim=c(1,length(allQs[[1]])),main='250bp chloro Read 1',xlab='Position',ylab='Average quality per sample')
+  lapply(newR1s_250,function(xx)lines(1:length(xx),xx,col='#00000044'))
+  plot(1,1,type='n',ylim=c(0,40),xlim=c(1,length(allQs[[1]])),main='250bp chloro Read 2',xlab='Position',ylab='Average quality per sample')
+  lapply(newR2s_250,function(xx)lines(1:length(xx),xx,col='#00000044'))
 dev.off()
 
 r1<-read.fastq('data/Weimin_10_13_16_2/PA1044matK-bd_S51_L001_R1_001.fastq.gz')
