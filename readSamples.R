280e7ba4b7da17c776802b63c2376e6bbc334065
sherrillmix <sherrillmix@users.noreply.github.com>
2017-03-13 15:51:19 -0400

    Add TL binary

--- readSamples.R
+++ readSamples.R
@@ -5,6 +5,7 @@ samples$bonobo<-samples$Species=='Pan paniscus'
 samples$area<-sub('[0-9]+$','',samples$Code)
 samples$area[samples$Site=='TL']<-'TL-E'
 samples$area[samples$Site=='TL (West)']<-'TL-W'
+samples$isTL<-samples$area %in% c('TL-E','TL-W')
 samples$rDate<-mdy(samples$Date)
 samples$month<-month(samples$rDate)
 #arbitrary seasons
