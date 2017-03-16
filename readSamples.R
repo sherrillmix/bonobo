580062acdf2a5447c11b860d1dbbe27cebe4744f
sherrillmix <sherrillmix@users.noreply.github.com>
2017-03-15 22:48:45 -0400

    Add north area

--- readSamples.R
+++ readSamples.R
@@ -8,6 +8,11 @@ samples$area[samples$Site=='TL (West)']<-'TL-W'
 samples$isTL<-samples$area %in% c('TL-E','TL-W')
 samples$rDate<-mdy(samples$Date)
 samples$month<-month(samples$rDate)
+samples$lat<-NA
+samples$lat[samples$isTL]<-as.numeric(gsub('[^0-9.]','',samples$Lat[samples$isTL]))
+samples$tlNorth<-samples$isTL&samples$lat<2.55
+samples$area2<-samples$area
+samples$area2[samples$tlNorth]<-'TL-NE'
 #arbitrary seasons
 samples$season<-as.character(ceiling(samples$month/3))
 rownames(samples)<-samples$Code
