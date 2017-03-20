a8271f8b86de5b6ef6ca47727872d8cb519e9cac
sherrillmix <sherrillmix@users.noreply.github.com>
2017-03-20 15:27:13 -0400

    Add shannon diversity

--- 16s/rarefyOtus.R
+++ 16s/rarefyOtus.R
@@ -1,33 +1,85 @@
+library(dnar)
 if(!exists('otuTab'))source('runQiime.R')
 
 
 medianCI<-function(xx,na.rm=TRUE){
   if(na.rm)xx<-xx[!is.na(xx)]
-  return(sort(xx)[qbinom(c(.025,.975), length(xx), 0.5)])
+  return(unname(sort(xx)[qbinom(c(.025,.975), length(xx), 0.5)]))
 }
 
 rareSteps<-seq(100,15000,100)
 rareCurves<-apply(otuTab[,samples$name[samples$isEnough]],2,rareEquation,rareSteps)
+
 groupings<-paste(samples$Species[samples$isEnough],ifelse(samples$malaria[samples$isEnough],'Plasmodium positive','Plasmodium negative'))
 groupCurves<-lapply(unique(groupings),function(xx)t(apply(rareCurves[,groupings==xx],1,quantile,c(.975,.5,.025),na.rm=TRUE)))
 groupCI<-lapply(unique(groupings),function(xx)t(apply(rareCurves[,groupings==xx],1,medianCI)))
 names(groupCurves)<-names(groupCI)<-unique(groupings)
 groupCols<-rainbow.lab(length(groupCurves),alpha=.9)
-groupCols2<-rainbow.lab(length(groupCurves),alpha=.2)
+groupCols2<-rainbow.lab(length(groupCurves),alpha=.25)
 names(groupCols)<-names(groupCols2)<-unique(groupings)
+
+groupings2<-paste(ifelse(grepl('^TL',samples$area[samples$isEnough]),'TL2 ',ifelse(samples$bonobo[samples$isEnough],'Non-endemic ','')),samples$Species[samples$isEnough],sep='')
+group2Curves<-lapply(unique(groupings2),function(xx)t(apply(rareCurves[,groupings2==xx],1,quantile,c(.975,.5,.025),na.rm=TRUE)))
+group2CI<-lapply(unique(groupings2),function(xx)t(apply(rareCurves[,groupings2==xx],1,medianCI)))
+names(group2Curves)<-names(group2CI)<-unique(groupings2)
+group2Cols<-rainbow.lab(length(group2Curves),alpha=.9,start=1,end=-2)
+group2Cols2<-rainbow.lab(length(group2Curves),alpha=.25,start=1,end=-2)
+names(group2Cols)<-names(group2Cols2)<-unique(groupings2)
+
+
 pdf('out/rare.pdf',width=6,height=6)
   par(mar=c(4,4,.1,.2))
+  #species and malaria
   plot(1,1,type='n',xlim=range(rareSteps),ylim=range(do.call(rbind,groupCurves)),xlab='Number of reads',ylab='Number of OTUs',las=1)
   for(ii in names(groupCurves)){
     polygon(c(rareSteps,rev(rareSteps)),c(groupCI[[ii]][,1],rev(groupCI[[ii]][,2])),col=groupCols2[ii],border=NA)
   }
   for(ii in names(groupCurves)){
-    #polygon(c(rareSteps,rev(rareSteps)),c(groupCurves[[ii]][,'97.5%'],rev(groupCurves[[ii]][,'2.5%'])),col=groupCols2[ii],border=NA)
     lines(rareSteps,groupCurves[[ii]][,'50%'],col=groupCols[ii],lwd=3)
   }
   legend('topleft',names(groupCols),lwd=2,col=groupCols,inset=.01)
+  #TL vs rest
+  plot(1,1,type='n',xlim=range(rareSteps),ylim=range(do.call(rbind,group2Curves)),xlab='Number of reads',ylab='Number of OTUs',las=1)
+  for(ii in names(group2Curves)){
+    polygon(c(rareSteps,rev(rareSteps)),c(group2CI[[ii]][,1],rev(group2CI[[ii]][,2])),col=group2Cols2[ii],border=NA)
+  }
+  for(ii in names(group2Curves)){
+    lines(rareSteps,group2Curves[[ii]][,'50%'],col=group2Cols[ii],lwd=3)
+  }
+  legend('topleft',names(group2Cols),lwd=2,col=group2Cols,inset=.01)
 dev.off()
 
+shannons<-apply(otuTab[,samples$name[samples$isEnough]],2,shannon)
+groupShannon<-sapply(unique(groupings),function(xx)median(shannons[groupings==xx]))
+shannonCI<-sapply(unique(groupings),function(xx)medianCI(shannons[groupings==xx]))
+group2Shannon<-sapply(unique(groupings2),function(xx)median(shannons[groupings2==xx]))
+shannon2CI<-sapply(unique(groupings2),function(xx)medianCI(shannons[groupings2==xx]))
+
+library(beeswarm)
+pdf('out/shannon.pdf',width=6,height=6)
+  par(mar=c(6.6,4,.1,4.2),lheight=.85)
+  plot(1,1,type='n',ylab='Shannon diversity',las=2,xlim=c(.5,length(unique(groupings))+.5),ylim=range(shannons),xaxt='n',xlab='',bty='l')
+  groupFac<-factor(sub(' Plas','\nPlas',groupings))
+  #xPos<-as.numeric(groupFac)+ave(shannons,groupFac,FUN=function(xx)swarmx(0,xx,cex=1.9)$x)
+  xPos<-as.numeric(groupFac)+offsetX(shannons,groupFac,width=.3)
+  width<-.45
+  segments(1:length(levels(groupFac))-width,groupShannon[sub('\n',' ',levels(groupFac))],1:length(levels(groupFac))+width,groupShannon[sub('\n',' ',levels(groupFac))],lwd=3,col=groupCols[sub('\n',' ',levels(groupFac))])
+  #rect(1:length(levels(groupFac))-width,shannonCI[1,sub('\n',' ',levels(groupFac))],1:length(levels(groupFac))+width,shannonCI[2,sub('\n',' ',levels(groupFac))],lwd=2,col=NA,border=groupCols[sub('\n',' ',levels(groupFac))])
+  rect(1:length(levels(groupFac))-width,shannonCI[1,sub('\n',' ',levels(groupFac))],1:length(levels(groupFac))+width,shannonCI[2,sub('\n',' ',levels(groupFac))],lwd=2,border=NA,col=groupCols2[sub('\n',' ',levels(groupFac))])
+  points(xPos,shannons,pch=21,bg=groupCols[groupings],cex=1.7)
+    #,shannons,,pch=21,bg=groupCols[groupings],cex=1.5
+  #vpPlot(factor(groupings2,levels=unique(groupings2)),shannons,ylab='Shannon diversity',las=2,pch=21,bg=group2Cols[groupings2],cex=1.5)
+  slantAxis(1,1:length(levels(groupFac)),levels(groupFac),srt=-30)
+  plot(1,1,type='n',ylab='Shannon diversity',las=2,xlim=c(.5,length(unique(groupings2))+.5),ylim=range(shannons),xaxt='n',xlab='',bty='l')
+  groupFac2<-factor(groupings2,levels=unique(groupings2))
+  #xPos<-as.numeric(groupFac2)+ave(shannons,groupFac2,FUN=function(xx)swarmx(0,xx,cex=1.9)$x)
+  xPos<-as.numeric(groupFac2)+offsetX(shannons,groupFac2,width=.3)
+  width<-.45
+  segments(1:length(levels(groupFac2))-width,group2Shannon[sub('\n',' ',levels(groupFac2))],1:length(levels(groupFac2))+width,group2Shannon[sub('\n',' ',levels(groupFac2))],lwd=3,col=group2Cols[sub('\n',' ',levels(groupFac2))])
+  rect(1:length(levels(groupFac2))-width,shannon2CI[1,sub('\n',' ',levels(groupFac2))],1:length(levels(groupFac2))+width,shannon2CI[2,sub('\n',' ',levels(groupFac2))],lwd=2,border=NA,col=group2Cols2[sub('\n',' ',levels(groupFac2))])
+  points(xPos,shannons,pch=21,bg=group2Cols[groupings2],cex=1.7)
+  slantAxis(1,1:length(levels(groupFac2)),levels(groupFac2),srt=-30)
+dev.off()
 
 
 moreThanProp<-apply(otuProp[,samples$name[samples$isEnough]],1,max,na.rm=TRUE)>.02
@@ -40,3 +92,5 @@ pdf('out/heat.pdf',height=20,width=20)
   heatmap(plotProp,col=cols,breaks=breaks,scale='none',margin=c(8,5),Rowv=NA)
   insetScale(round(breaks,6),cols,c(.955,.75,.97,.99),label='Proportion')
 dev.off()
+
+
