1b28ebaf4f711c11f8c53d696cdd62ed5ad29227
sherrillmix <sherrillmix@users.noreply.github.com>
2017-04-24 17:01:51 -0400

    Load in swarm data

--- loadData.R
+++ loadData.R
@@ -7,7 +7,7 @@ fastqs<-list.files('data/','_R[12]_.*\\.fastq\\.gz$',recursive=TRUE,full.names=T
 fastqs<-fastqs[!grepl('Undetermined',fastqs)]
 primers<-sub('.*(matK|rbcL).*_R([0-9]+)_.*','\\1\\2',basename(fastqs))
 
-for(ii in unique(primers)){
+swarmData<-lapply(unique(primers),function(ii){
   message(ii)
   outMat<-sprintf('work/swarm/%s.Rdat',ii)
   outFa<-sprintf('work/swarm/%s.fa.gz',ii)
@@ -15,4 +15,18 @@ for(ii in unique(primers)){
   outHits<-sprintf('work/swarm/%s_allHits.csv',ii)
   if(!file.exists(outMat)|!file.exists(outFa))source('makeOtus.R')
   if(!file.exists(outTaxa)|!file.exists(outHits))source('parseBlast.R')
-}
+  tmp<-environment()
+  load(outMat,tmp)
+  swarmOtus<-get('swarmOtus',tmp)
+  rm(tmp)
+  swarmSeqs<-read.fa(outFa)
+  rownames(swarmSeqs)<-swarmSeqs$name
+  swarmTaxa<-read.csv(outTaxa,row.names=1)
+  swarmTaxa$seq<-swarmSeqs[rownames(swarmTaxa),'seq']
+  return(list('otus'=swarmOtus,'taxa'=swarmTaxa))
+})
+names(swarmData)<-unique(primers)
+
+nReads<-lapply(swarmData,function(xx)apply(xx[['otus']],1,sum))
+isEnough<-sapply(nReads,function(xx)xx>5000)
+
