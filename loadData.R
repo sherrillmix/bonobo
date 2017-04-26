8f4dbf9d0db92c46b67a1688310262d965651239
sherrillmix <sherrillmix@users.noreply.github.com>
2017-04-26 16:07:31 -0400

    Additional internal calculations

--- loadData.R
+++ loadData.R
@@ -1,12 +1,14 @@
 library(dnar)
 library(parallel)
 source('functions.R')
+source('16s/functions.R')
 source('readSamples.R')
 
 fastqs<-list.files('data/','_R[12]_.*\\.fastq\\.gz$',recursive=TRUE,full.names=TRUE)
 fastqs<-fastqs[!grepl('Undetermined',fastqs)]
 primers<-sub('.*(matK|rbcL).*_R([0-9]+)_.*','\\1\\2',basename(fastqs))
 
+nReadCut<-5000
 swarmData<-lapply(unique(primers),function(ii){
   message(ii)
   outMat<-sprintf('work/swarm/%s.Rdat',ii)
@@ -21,13 +23,16 @@ swarmData<-lapply(unique(primers),function(ii){
   rm(tmp)
   swarmSeqs<-read.fa(outFa)
   rownames(swarmSeqs)<-swarmSeqs$name
-  swarmTaxa<-read.csv(outTaxa,row.names=1)
+  swarmTaxa<-read.csv(outTaxa,row.names=1,stringsAsFactors=FALSE)
   swarmTaxa$seq<-swarmSeqs[rownames(swarmTaxa),'seq']
-  props<-apply(swarmOtus,1,function(xx)xx/sum(xx))
-  return(list('otus'=swarmOtus,'props'=props,'taxa'=swarmTaxa))
+  swarmTaxa$bestId<-ave(swarmTaxa$best,naReplace(swarmTaxa$best,'__NAFILLER__'),FUN=function(x){sprintf('%s #%d',ifelse(is.na(x),'Unknown',x),1:length(x))})
+  #trashing singletons
+  swarmOtus<-swarmOtus[,apply(swarmOtus,2,sum)>1]
+  props<-t(apply(swarmOtus,1,function(xx)xx/sum(xx)))
+  nReads<-apply(swarmOtus,1,sum)
+  isEnough<-nReads>nReadCut
+  return(list('otus'=swarmOtus,'props'=props,'taxa'=swarmTaxa,'nReads'=nReads,'isEnough'=isEnough))
 })
 names(swarmData)<-unique(primers)
 
-nReads<-lapply(swarmData,function(xx)apply(xx[['otus']],1,sum))
-isEnough<-lapply(nReads,function(xx)xx>5000)
 
