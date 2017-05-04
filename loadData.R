715843f620b0b28cc6dc8d7a483d202ded6c5ad8
sherrillmix <sherrillmix@users.noreply.github.com>
2017-05-04 12:37:13 -0400

    Load paired data

--- loadData.R
+++ loadData.R
@@ -4,17 +4,18 @@ source('functions.R')
 source('16s/functions.R')
 source('readSamples.R')
 
-fastqs<-list.files('data/','_R[12]_.*\\.fastq\\.gz$',recursive=TRUE,full.names=TRUE)
-fastqs<-fastqs[!grepl('Undetermined',fastqs)]
-primers<-sub('.*(matK|rbcL).*_R([0-9]+)_.*','\\1\\2',basename(fastqs))
+#fastqs<-list.files('data/','_R[12]_.*\\.fastq\\.gz$',recursive=TRUE,full.names=TRUE)
+#fastqs<-fastqs[!grepl('Undetermined',fastqs)]
+#primers<-sub('.*(matK|rbcL).*_R([0-9]+)_.*','\\1\\2',basename(fastqs))
+primers<-c('matK','rbcL')
 
 nReadCut<-5000
 swarmData<-lapply(unique(primers),function(ii){
   message(ii)
-  outMat<-sprintf('work/swarm/%s.Rdat',ii)
-  outFa<-sprintf('work/swarm/%s.fa.gz',ii)
-  outTaxa<-sprintf('work/swarm/%s_taxa.csv',ii)
-  outHits<-sprintf('work/swarm/%s_allHits.csv',ii)
+  outMat<-sprintf('work/swarmPair/%s.Rdat',ii)
+  outFa<-sprintf('work/swarmPair/%s.fa.gz',ii)
+  outTaxa<-sprintf('work/swarmPair/%s_taxa.csv',ii)
+  outHits<-sprintf('work/swarmPair/%s_allHits.csv',ii)
   if(!file.exists(outMat)|!file.exists(outFa))source('makeOtus.R')
   if(!file.exists(outTaxa)|!file.exists(outHits))source('parseBlast.R')
   tmp<-environment()
