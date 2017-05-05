f219a83a844464af1b0a8d8dd3df52a1e75d39e2
sherrillmix <sherrillmix@users.noreply.github.com>
2017-05-05 08:56:21 -0400

    Fix up cluster plots

--- makeClusters.R
+++ makeClusters.R
@@ -14,7 +14,10 @@ for(ii in names(swarmData)){
   brayPca<-pcoa(brayDist)
   tsneBray<-Rtsne(brayDist,is_distance=TRUE,verbose=TRUE,perplexity=15,max_iter=3000)
 
+  importance<-brayPca$values$Relative_eig
+  colnames(brayPca$vectors)<-sprintf('Principal coordinate %d (%d%% of variance)',1:length(importance),round(importance*100))[1:ncol(brayPca$vectors)]
   selectSamples<-samples[rownames(plotProp),]
+  selectSamples<-selectSamples[order(selectSamples$bonobo,selectSamples$area2,selectSamples$malaria),]
   colorBrew<-c('#e41a1cBB','#377eb8BB','#4daf4aBB','#984ea3BB','#ff7f00BB','#ffff33BB','#a65628BB','#f781bfBB','#999999BB','#88ddffBB')
   nArea<-length(unique(selectSamples$area2))
   if(nArea>length(colorBrew))stop('Need to adjust colors for more areas')
@@ -29,18 +32,20 @@ for(ii in names(swarmData)){
   names(speciesCols)<-names(speciesPch)<-unique(selectSamples$chimpBonobo)
 
   predictors<-model.matrix(~0+Species+malaria+SIV+area,selectSamples)
-  pos<-my.biplot.pcoa(brayPca,predictors,plot.axes=1:2,pch=speciesPch[selectSamples$chimpBonobo],bg=areaCols[selectSamples$area2],col=malariaCols[selectSamples$malaria+1],cex=2.2,lwd=2.5,mar=c(4,4,1.5,10),arrowsFilter=Inf)
-  points(pos[selectSamples$SIV=='Pos',],col='#FF000099',cex=2.7,lwd=2)
-  legend(
-    par('usr')[2]+.01*diff(par('usr')[1:2]), 
-    mean(par('usr')[3:4]),
-    c(names(malariaCols),names(areaCols),names(speciesPch),'SIVPos'),
-    col=c(malariaCols,rep(malariaCols,c(length(areaCols),length(speciesPch))),'#FF000099'),
-    pch=c(rep(21,length(malariaCols)),speciesPch[areaPch],speciesPch,1),
-    pt.bg=c(rep(NA,length(malariaCols)),areaCols,rep(NA,length(speciesPch)),NA),
-    inset=.01,pt.lwd=3,pt.cex=2.5,
-    xjust=0,xpd=NA
-  )
+  #pos<-my.biplot.pcoa(brayPca,predictors,plot.axes=1:2,pch=speciesPch[selectSamples$chimpBonobo],bg=areaCols[selectSamples$area2],col=malariaCols[selectSamples$malaria+1],cex=2.2,lwd=2.5,mar=c(4,4,1.5,10),arrowsFilter=Inf)
+  #legend(
+    #par('usr')[2]+.01*diff(par('usr')[1:2]), 
+    #mean(par('usr')[3:4]),
+    #c(names(malariaCols),names(areaCols),names(speciesPch)),
+    #col=c(malariaCols,rep(malariaCols,c(length(areaCols),length(speciesPch)))),
+    #pch=c(rep(21,length(malariaCols)),speciesPch[areaPch],speciesPch),
+    #pt.bg=c(rep(NA,length(malariaCols)),areaCols,rep(NA,length(speciesPch))),
+    #inset=.01,pt.lwd=3,pt.cex=2.5,
+    #xjust=0,xpd=NA
+  #)
+
+    pos<-my.biplot.pcoa(brayPca,predictors,plot.axes=1:2,pch=21,bg=speciesCols[selectSamples$chimpBonobo],col=malariaCols[selectSamples$malaria+1],cex=2.25,lwd=4,arrowsFilter=Inf)
+    legend('bottomright',as.vector(outer(names(speciesCols),names(malariaCols),paste,sep=' ')),col=as.vector(malariaCols[outer(names(speciesCols),names(malariaCols),function(x,y)y)]),pch=21,pt.bg=as.vector(speciesCols[outer(names(speciesCols),names(malariaCols),function(x,y)x)]),inset=.01,pt.lwd=4,pt.cex=2.5)
   title(main=sprintf('%s PCoA PC %d and %d',ii,1,2))
 
   par(mar=c(4,4,1.5,10))
