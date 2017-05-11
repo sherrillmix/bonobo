e30c4ced2eb895f227d5044ef29bbfa9bce4595d
sherrillmix <sherrillmix@users.noreply.github.com>
2017-05-11 17:03:50 -0400

    Add additional adonis. Switch to binary bray

--- makeClusters.R
+++ makeClusters.R
@@ -3,13 +3,15 @@ library(phyloseq)
 library(ape)
 library(vipor)
 library(Rtsne)
+library(vegan)
 source('16s/myBiplot.R')
 
+interactAdonis<-plantAdonis<-chimpAdonis<-bonoboAdonis<-list()
 for(ii in names(swarmData)){
   plotProp<-swarmData[[ii]][['props']][swarmData[[ii]][['isEnough']]&rownames(swarmData[[ii]][['props']]) %in% rownames(samples),]
   phyOtuW<-otu_table(plotProp,taxa_are_rows=FALSE)
   qiimeDataW<-phyloseq(otu_table=phyOtuW)
-  brayDist<-distance(qiimeDataW,'bray')
+  brayDist<-distance(qiimeDataW,'bray',binary=TRUE)
   brayPca<-pcoa(brayDist)
   tsneBray<-Rtsne(brayDist,is_distance=TRUE,verbose=TRUE,perplexity=15,max_iter=3000)
 
@@ -25,12 +27,14 @@ for(ii in names(swarmData)){
   areaPch<-sapply(names(areaCols),function(x)mostAbundant(selectSamples$chimpBonobo[selectSamples$area2==x]))
   #malariaCols3<-c(NA,'#000000CC')
   malariaCols3<-c('#00000022','#000000CC')
+  mediumMalariaCol<-'#00000077'
   malariaCols<-c('#00000022','#000000CC')
   malariaCols2<-rainbow.lab(2,alpha=.9,lightMultiple=.7)
   names(malariaCols3)<-names(malariaCols2)<-names(malariaCols)<-c('Laverania negative','Laverania positive')
   speciesPch<-20+1:length(unique(selectSamples$Species))
   #speciesCols<-c('#FF0000CC','#0000FFCC')
-  speciesCols<-rainbow.lab(length(unique(selectSamples$Species)),start=-2,end=2,alpha=.9,lightMultiple=.8)
+  #speciesCols<-rainbow.lab(length(unique(selectSamples$Species)),start=-2,end=2,alpha=.9,lightMultiple=.8)
+  speciesCols<-rainbow.lab(length(unique(selectSamples$Species)),start=-2,end=1,alpha=.8,lightMultiple=.8)
   names(speciesCols)<-names(speciesPch)<-sort(unique(selectSamples$chimpBonobo))
 
   predictors<-model.matrix(~0+Species+malaria+SIV+area,selectSamples)
@@ -46,25 +50,37 @@ for(ii in names(swarmData)){
     #xjust=0,xpd=NA
   #)
 
-  pdf(sprintf('out/pcoa_%s.pdf',ii),width=7,height=7)
-    pos<-my.biplot.pcoa(brayPca,predictors,plot.axes=1:2,pch=21,bg=speciesCols[selectSamples$chimpBonobo],col=malariaCols3[selectSamples$malaria+1],cex=2.25,lwd=4,arrowsFilter=Inf,las=1,mgp=c(2.75,.75,0))
-    legend('bottomright',as.vector(outer(names(speciesCols),names(malariaCols3),paste,sep=' ')),col=as.vector(malariaCols3[outer(names(speciesCols),names(malariaCols3),function(x,y)y)]),pch=21,pt.bg=as.vector(speciesCols[outer(names(speciesCols),names(malariaCols3),function(x,y)x)]),inset=.01,pt.lwd=4,pt.cex=2.5,bty='n')
+  print(table(selectSamples$malaria,selectSamples$chimpBonobo))
+  pdf(sprintf('out/pcoa_%s.pdf',ii),width=6,height=6)
+    pos<-my.biplot.pcoa(brayPca,predictors,plot.axes=1:2,pch=21,bg=speciesCols[selectSamples$chimpBonobo],col=malariaCols3[selectSamples$malaria+1],cex=2.25,lwd=4,arrowsFilter=Inf,las=1,mgp=c(2.75,.75,0),sameAxis=FALSE,bty='l',type='n')
+    points(pos[!selectSamples$malaria,],col=malariaCols3[1],cex=2.25,lwd=4,bg=speciesCols[selectSamples[!selectSamples$malaria,'chimpBonobo']],pch=21)
+    points(pos[selectSamples$malaria,],col=malariaCols3[2],cex=2.25,lwd=4,bg=speciesCols[selectSamples[selectSamples$malaria,'chimpBonobo']],pch=21)
+    #legend('bottomright',as.vector(outer(names(speciesCols),names(malariaCols3),paste,sep=' ')),col=as.vector(malariaCols3[outer(names(speciesCols),names(malariaCols3),function(x,y)y)]),pch=21,pt.bg=as.vector(speciesCols[outer(names(speciesCols),names(malariaCols3),function(x,y)x)]),inset=.01,pt.lwd=4,pt.cex=2.5,bty='n')
     title(main=sprintf('%s',ii,1,2))
   dev.off()
 
-  pdf(sprintf('out/tsne_%s.pdf',ii),width=9,height=7)
+  pdf(sprintf('out/tsne_%s.pdf',ii),width=10,height=8)
     par(mar=c(4,4,1.5,10))
-    plot(tsneBray$Y,pch=speciesPch[selectSamples$chimpBonobo],bg=areaCols[selectSamples$area2],col=malariaCols[selectSamples$malaria+1],cex=2.2,lwd=2.5,ylab='t-SNE 2',xlab='t-SNE 1',main=sprintf('%s t-SNE',ii))
+    plot(tsneBray$Y,pch=speciesPch[selectSamples$chimpBonobo],bg=areaCols[selectSamples$area2],col=malariaCols[selectSamples$malaria+1],cex=2.5,lwd=3,ylab='t-SNE 2',xlab='t-SNE 1',main=sprintf('%s',ii),bty='l',las=1)
     legend(
       par('usr')[2]+.01*diff(par('usr')[1:2]), 
       mean(par('usr')[3:4]),
       c(names(malariaCols),names(areaCols),names(speciesPch)),
-      col=c(malariaCols,rep(malariaCols,c(length(areaCols),length(speciesPch)))),
+      col=c(malariaCols,rep(c(malariaCols[1],mediumMalariaCol),c(length(areaCols),length(speciesPch)))),
       pch=c(rep(21,length(malariaCols)),speciesPch[areaPch],speciesPch),
       pt.bg=c(rep(NA,length(malariaCols)),areaCols,rep(NA,length(speciesPch))),
       inset=.01,pt.lwd=3,pt.cex=2.5,
-      xjust=0,xpd=NA
+      xjust=0,xpd=NA,bty='n'
     )
   dev.off()
 
+  ss<-samples[labels(brayDist),]
+  plantAdonis[[ii]]<-cacheOperation(sprintf('work/adonis_%s.Rdat',ii),adonis,brayDist~bonobo+area2+malaria,data=ss,permutations=1e7,parallel=5)
+  interactAdonis[[ii]]<-cacheOperation(sprintf('work/interactAdonis_%s.Rdat',ii),adonis,brayDist~bonobo+area2+malaria*bonobo,data=ss,permutations=1e7,parallel=5)
+  chimpDist<-as.matrix(brayDist)
+  chimpDist<-as.dist(chimpDist[!ss$bonobo,!ss$bonobo])
+  chimpAdonis[[ii]]<-cacheOperation(sprintf('work/adonisChimp_%s.Rdat',ii),adonis,chimpDist~area2+malaria,data=ss[!ss$bonobo,],permutations=1e7,parallel=5)
+  bonoboDist<-as.matrix(brayDist)
+  bonoboDist<-as.dist(bonoboDist[ss$bonobo,ss$bonobo])
+  bonoboAdonis[[ii]]<-cacheOperation(sprintf('work/adonisBonobo_%s.Rdat',ii),adonis,bonoboDist~area2+malaria,data=ss[ss$bonobo,],permutations=1e7,parallel=5)
 }
