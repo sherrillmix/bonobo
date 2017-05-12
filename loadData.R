3abde083ca6937a27e63cca6980d7f05cc113c94
sherrillmix <sherrillmix@users.noreply.github.com>
2017-05-12 17:01:52 -0400

    Subsample otus. Add taxa prefix

--- loadData.R
+++ loadData.R
@@ -16,6 +16,7 @@ swarmData<-lapply(unique(primers),function(ii){
   outFa<-sprintf('work/swarmPair/%s.fa.gz',ii)
   outTaxa<-sprintf('work/swarmPair/%s_taxa.csv',ii)
   outHits<-sprintf('work/swarmPair/%s_allHits.csv',ii)
+  outAlign<-sprintf('work/swarmPair/%s_align.fa.gz',ii)
   if(!file.exists(outMat)|!file.exists(outFa))source('makeOtus.R')
   if(!file.exists(outTaxa)|!file.exists(outHits))source('parseBlast.R')
   tmp<-environment()
@@ -27,14 +28,14 @@ swarmData<-lapply(unique(primers),function(ii){
   rownames(swarmSeqs)<-swarmSeqs$name
   swarmTaxa<-read.csv(outTaxa,row.names=1,stringsAsFactors=FALSE)
   swarmTaxa$seq<-swarmSeqs[rownames(swarmTaxa),'seq']
-  swarmTaxa$bestId<-ave(swarmTaxa$best,naReplace(swarmTaxa$best,'__NAFILLER__'),FUN=function(x){sprintf('%s #%d',ifelse(is.na(x),'Unknown',x),1:length(x))})
+  swarmTaxa$bestAnnot<-apply(swarmTaxa[,c('phylum','class','order','family','genus','species')],1,function(x)ifelse(is.na(x),x,sprintf('%s_%s',substring(names(x),1,1),x))[max(c(1,which(!is.na(x))))])
+  swarmTaxa$bestId<-ave(swarmTaxa$bestAnnot,naReplace(swarmTaxa$best,'__NAFILLER__'),FUN=function(x){sprintf('%s #%d',ifelse(is.na(x),'Unknown',x),1:length(x))})
   #trashing singletons
   swarmOtus<-swarmOtus[,apply(swarmOtus,2,sum)>1]
   props<-t(apply(swarmOtus,1,function(xx)xx/sum(xx)))
   nReads<-apply(swarmOtus,1,sum)
   isEnough<-nReads>nReadCut
-  return(list('otus'=swarmOtus,'props'=props,'taxa'=swarmTaxa,'nReads'=nReads,'isEnough'=isEnough))
+  subsampledOtus<-cacheOperation(sprintf('work/%s_rarefyOtus.Rdat',ii),apply,swarmOtus,2,function(xx)if(sum(xx)<nReadCut)rep(NA,length(xx)) else rarefyCounts(xx,nReadCut))
+  return(list('otus'=swarmOtus,'props'=props,'rare'=subsampledOtus,'taxa'=swarmTaxa,'nReads'=nReads,'isEnough'=isEnough))
 })
 names(swarmData)<-unique(primers)
-
-
