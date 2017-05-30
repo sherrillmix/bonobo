286279b674ad2804de2107bdceb79932aa86d9d9
sherrillmix <sherrillmix@users.noreply.github.com>
2017-05-30 17:48:40 -0400

    Adjust figure title

--- 16s/plotHeat.R
+++ 16s/plotHeat.R
@@ -18,7 +18,7 @@ plotProp<-plotProp[,rev(maxTree$labels[maxTree$order])]
 breaks<-c(-1e-6,seq(min(plotProp[plotProp>0])-1e-10,max(plotProp)+1e-10,length.out=100))
 breaks2<-c(-1e-6,seq(min(maxProp[maxProp>0])-1e-10,max(maxProp)+1e-10,length.out=100))
 #plotProp<-plotProp[,order(apply(plotProp,2,mean))]
-pdf('out/Fig.S5.pdf',height=30,width=20)
+pdf('out/Fig.S8.pdf',height=30,width=20)
   par(mfrow=c(2,1),mar=c(12,.1,3.5,14))
   image(1:ncol(plotProp),1:nrow(plotProp),t(plotProp),col=cols,breaks=breaks,xlab='',ylab='',xaxt='n',yaxt='n')
   text(grconvertX(.005, "nfc", "user"),grconvertY(.995, "nfc", "user"),'A)',xpd=NA,cex=3,adj=0:1)
@@ -76,7 +76,7 @@ effectSplitIk<-tmp[[2]]
 pdf('out/splitOtus.pdf',height=13,width=15)
   par(mar=c(11.5,.1,3,13.5),lheight=.7)
   plotHeat(selectProp,breaks2,cols,yaxt='n')
-  title(main='TL + vs TL - (q<.05)')
+  #title(main='TL + vs TL - (q<.05)')
   abline(v=effectSplit-.5)
   metadata<-samples[rownames(selectProp),c('chimpBonobo','area2','plasmoPM','Code')]
   colnames(metadata)<-c('Species','Site','Laverania','Sample')
@@ -96,6 +96,6 @@ pdf('out/splitOtus.pdf',height=13,width=15)
   #heatmap(selectPropTl,col=cols,breaks=breaks2,scale='none',margin=c(16,6),Rowv=NA)
   #insetScale(round(breaks2,6),cols,c(.955,.75,.97,.99),label='Proportion of OTU maximum')
 dev.off()
-system('pdftk out/splitOtus.pdf cat 1 output out/Fig.S7.pdf')
+system('pdftk out/splitOtus.pdf cat 1 output out/Fig.S10.pdf')
 
 
