8540501b90058f234d36c16f56f66c07a21c9bea
sherrillmix <sherrillmix@users.noreply.github.com>
2017-08-01 16:59:05 -0400

    Remove unnecessary file

--- loadData.R
+++ loadData.R
@@ -6,16 +6,13 @@ source('readSamples.R')
 primers<-c('matK','rbcL')
 
 nReadCut<-5000
-swarmData<-lapply(unique(primers),function(ii){
+swarmData<-lapply(primers,function(ii){
   message(ii)
   outMat<-sprintf('work/swarmPair/%s.Rdat',ii)
   outFa<-sprintf('work/swarmPair/%s.fa.gz',ii)
   outTaxa<-sprintf('work/swarmPair/%s_taxa.csv',ii)
-  outHits<-sprintf('work/swarmPair/%s_allHits.csv',ii)
   outAlign<-sprintf('work/swarmPair/%s_align.fa.gz',ii)
   outTree<-sprintf('work/swarmPair/%s_align.tre',ii)
-  if(!file.exists(outMat)|!file.exists(outFa))source('makeOtus.R')
-  if(!file.exists(outTaxa)|!file.exists(outHits))source('parseBlast.R')
   tmp<-environment()
   load(outMat,tmp)
   swarmOtus<-get('swarmOtus',tmp)
@@ -27,7 +24,7 @@ swarmData<-lapply(unique(primers),function(ii){
   swarmTaxa$seq<-swarmSeqs[rownames(swarmTaxa),'seq']
   swarmTaxa$bestAnnot<-apply(swarmTaxa[,c('phylum','class','order','family','genus','species')],1,function(x)ifelse(is.na(x),x,sprintf('%s_%s',substring(names(x),1,1),x))[max(c(1,which(!is.na(x))))])
   swarmTaxa$bestId<-ave(swarmTaxa$bestAnnot,naReplace(swarmTaxa$best,'__NAFILLER__'),FUN=function(x){sprintf('%s #%d',ifelse(is.na(x),'Unknown',x),1:length(x))})
-  #trashing singletons
+  #excluding singletons
   swarmOtus<-swarmOtus[,apply(swarmOtus,2,sum)>1]
   props<-t(apply(swarmOtus,1,function(xx)xx/sum(xx)))
   nReads<-apply(swarmOtus,1,sum)
